49059e5d48b0e5332d5c5b920f9b94c1f473751b
Ahmadou Lo <ahmadou19@gmail.com>
2019-11-08 16:46:43 +0100

    test du CRUD Personne

--- src/presentation/Lanceur.java
+++ src/presentation/Lanceur.java
@@ -1,5 +1,8 @@
 package presentation;
 
+import java.util.Date;
+
+import metier.Personne;
 import service.Iservice;
 import service.ServiceImpl;
 
@@ -10,8 +13,24 @@ public class Lanceur {
 		
 		Iservice iservice = new ServiceImpl();
 		ServiceImpl.base = iservice.init();
-		
+		 System.out.println("Affiche Personne ");
 		iservice.affichePersonnes();
+		
+		// recuperer la personne 2"
+		Personne p = iservice.getPersonne(ServiceImpl.base, 2);
+		System.out.println(p.toString());
+		
+		//supprimer une personne 
+		iservice.deletePersonne(ServiceImpl.base, 1);
+		System.out.println("apres Delete ");
+		iservice.affichePersonnes();
+		
+		// modifier Personne 
+		iservice.updatePersonne(ServiceImpl.base, p, "Zegrour", "Youssef", new Date("1902/12/28"));
+		
+		// recuperer la personne apres modification"
+				 p = iservice.getPersonne(ServiceImpl.base, 2);
+				System.out.println(p.toString());
 
 	}
 
