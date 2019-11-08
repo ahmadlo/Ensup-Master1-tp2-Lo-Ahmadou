20b884001eeddedef29ed50b82f69708a4c23e7b
Ahmadou Lo <ahmadou19@gmail.com>
2019-11-08 15:30:27 +0100

    Création de la classe Annonce

---
+++ src/metier/Annonce.java
@@ -0,0 +1,36 @@
+package metier;
+
+public class Annonce {
+
+	private int idAnnonce;
+	private String intitule;
+	private String detail;
+	private Double prix;
+	public int getIdAnnonce() {
+		return idAnnonce;
+	}
+	public void setIdAnnonce(int idAnnonce) {
+		this.idAnnonce = idAnnonce;
+	}
+	public String getIntitule() {
+		return intitule;
+	}
+	public void setIntitule(String intitule) {
+		this.intitule = intitule;
+	}
+	public String getDetail() {
+		return detail;
+	}
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+	public Double getPrix() {
+		return prix;
+	}
+	public void setPrix(Double prix) {
+		this.prix = prix;
+	}
+	
+	
+	
+}
